831682684dba96793603322e4455be74529f1778
Riya Bhardwaj <iriyabhardwaj@gmail.com>
2026-06-27 02:04:44 +0530

    Time: 0 ms (100%), Space: 12.6 MB (13.38%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>preSum(n);
+        preSum[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            preSum[i]=preSum[i-1]+nums[i];
+        }
+        return preSum;
+    }
+};
