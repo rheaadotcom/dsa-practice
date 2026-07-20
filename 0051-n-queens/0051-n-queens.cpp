584d4db623cea283cebc4189cf62d81d5b576d83
Riya Bhardwaj <iriyabhardwaj@gmail.com>
2026-07-21 02:07:09 +0530

    Time: 8 ms (15.39%), Space: 12.2 MB (18.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,42 +2,39 @@ class Solution {
 public:
 vector<vector<string>>ans;
 int N;
-    vector<vector<string>> solveNQueens(int n) {
-        N=n;
-       vector<string>board(n,string(n,'.'));
-       
-       solve(board,0); 
-       return ans;
-    }
-void solve(vector<string>& board,int row){
-    if(row>=N){
+ unordered_set<int> cols;
+    unordered_set<int> diagonals;
+    unordered_set<int> antidiagonals;
+    void solve(vector<string>& board,int row){
+        if(row>=N){
         ans.push_back(board);
         return;
-    }
-    for(int col=0;col<N;col++){
-        if(isValid(board,row,col)){
-        board[row][col]='Q';
-        solve(board,row+1);
-        board[row][col]='.';
         }
+    for(int col=0;col<N;col++){
+        int diagonal=row+col;
+        int antidiagonal=row-col;    
+
+    if(cols.find(col)!=cols.end() ||diagonals.find(diagonal)!=diagonals.end() ||antidiagonals.find(antidiagonal)!=antidiagonals.end() ){
+        continue;
+    }    
+    cols.insert(col);
+    diagonals.insert(diagonal);
+    antidiagonals.insert(antidiagonal);
+    board[row][col]='Q';
+    solve(board,row+1);
+    
+   
+    cols.erase(col);
+    diagonals.erase(diagonal);
+    antidiagonals.erase(antidiagonal);
+    board[row][col]='.';
+     
     }
-}
-bool isValid(vector<string>& board,int row,int col){
-    //check for upwards
-    for(int i=row-1;i>=0;i--){
-        if(board[i][col]=='Q')
-        return false;
     }
-    //check for left diagonals
-    for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-       if(board[i][j]=='Q')
-        return false;
+    vector<vector<string>> solveNQueens(int n) {
+        N=n;
+        vector<string>board(n,string(n,'.'));
+        solve(board,0);
+        return ans;
     }
-    //check for right diagonals
-    for(int i=row-1,j=col+1;i>=0 && j<N;i--,j++){
-        if(board[i][j]=='Q')
-        return false;
-    }
-    return true;
-}
 };
