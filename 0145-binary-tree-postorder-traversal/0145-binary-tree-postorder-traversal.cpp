dd76f2fef5650df1cf9664a7426644d9134ddb85
Riya Bhardwaj <iriyabhardwaj@gmail.com>
2026-07-30 01:58:57 +0530

    Time: 0 ms (100%), Space: 11 MB (74.27%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -6,29 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>postOrder;
-        if(root==NULL) return postOrder;
-        stack<TreeNode*>st1,st2;
-        
-        st1.push(root);
-        while(!st1.empty()){
-            root=st1.top();
-            st1.pop();
-            st2.push(root);
-            if(root->left !=NULL) st1.push(root->left);
-            if(root->right !=NULL) st1.push(root->right); 
-        }
-
-            while(!st2.empty()){
-                postOrder.push_back(st2.top()->val);
-                st2.pop();
+        TreeNode* cur = root;
+        vector<int> postOrder;
+        if (root == NULL)
+            return postOrder;
+        stack<TreeNode*> st;
+        while (cur != NULL || !st.empty()) {
+            if (cur != NULL) {
+                st.push(cur);
+                cur = cur->left;
+            } else {
+                TreeNode*temp=st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    postOrder.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                    temp=st.top();
+                    st.pop();
+                    postOrder.push_back(temp->val);
+                    }
+                }else{
+                    cur=temp;
+                }
             }
+        }
         return postOrder;
     }
 };
